371f752ade03ddebfaaac8351865c53c762ef09b
justusjukkala <justus.jukkala@student.saimia.fi>
2019-09-26 13:28:43 +0300

    ReadWriteExample done

--- ReadWriteExample/ReadWriteExample/Program.cs
+++ ReadWriteExample/ReadWriteExample/Program.cs
@@ -6,7 +6,15 @@ namespace ReadWriteExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ReadWriteExample");
+            Console.WriteLine("ReadWriteExample"); // Tulostaa näytölle vakio viestin ReadWriteExample
+            Console.Write("Moi, Kirjoita nimesi: "); // Tlostaa näytölle vakio viestin
+            string userInput = Console.ReadLine(); // Luetaan käyttäjän syöttämät merkit niin pitkään kunnes painetaan Enter 
+            // sijoitetaan merkit merkkijonomuuttujaan userInput
+            Console.WriteLine("-------------------"); //  Tulostaa näytölle vakioviestin
+            Console.WriteLine("Moi " + userInput + "!"); // Tulostaa näytölle vakioviestin ja muuttujan userInput arvon
+            Console.WriteLine("Moi {0}!", userInput);
+            Console.WriteLine($"Moi {userInput}!"); // Tulostuksen muotoilu ja muuttujien käyttö tulostuksen yhteydessä $-merkin avulla = string.Format()
+            
         }
     }
 }
