5fb153659e5f3ac60632cb59c23fb687b9261b5b
justusjukkala <justus.jukkala@student.saimia.fi>
2019-10-08 12:28:55 +0300

    IfTask2 Created

---
+++ IfTask2/IfTask2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IfTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Maatalousnäyttelyn lipunhinta");
+        }
+    }
+}
