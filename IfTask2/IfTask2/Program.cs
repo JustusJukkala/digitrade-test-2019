14ae5391740874a36741d4e48c47c49eeabe0eef
justusjukkala <justus.jukkala@student.saimia.fi>
2019-10-10 10:41:28 +0300

    IfTask2 completed

--- IfTask2/IfTask2/Program.cs
+++ IfTask2/IfTask2/Program.cs
@@ -12,25 +12,58 @@ namespace IfTask2
 
             int age = int.Parse(Console.ReadLine());
             // pitää viellä kysyä että onko varusmies, opiskelia, opiskelia mtk jäsenyydellä, mtk jäsen tai ei jäsenyyttä
-            int ticketPrice = 16;
+            int ticketPrice =16;
             double discount = 0;
 
             if (age < 7) // alle 7 vuotiaiden lapsien alennus
             {
                 discount = 1;
-                Console.WriteLine($"Hinta on {ticketPrice - (ticketPrice*discount)}!");
+                Console.WriteLine($"Hinta on {ticketPrice - (ticketPrice * discount)}!");
             }
             else if (age > 65) // Eläkeläisten alennus
-
             {
                 discount = 0.5;
                 Console.WriteLine($"Hinta on {ticketPrice - (ticketPrice * discount)}!");
             }
-            else if (age < 7 || age > 65) // muitten kuin lapsien ja eläkeläisten hinta
+            else if (age >= 7 && age <= 15) // 7-15 hinta
+            {
+                discount = 0.5;
+                Console.WriteLine($"Hinta on {ticketPrice - (ticketPrice * discount)}!");
+
+            }
+            else if (age < 15 || age > 65) // muitten kuin lapsien ja eläkeläisten hinta
             {
                 discount = 0;
                 Console.WriteLine($"Hinta on {ticketPrice - (ticketPrice * discount)}!");
             }
+            Console.WriteLine("Oletko varusmies, Opiskelia mtk:n jäsenyydellä, Pelkkä opiskelia, mtk jäsen tai joku muu?");
+
+            Console.WriteLine("Oletko varusmies? Kyllä/En");
+            string userInput = Console.ReadLine(); // kysytään seuraavaksi, että onko varusmies tai jonkin muu
+            if (userInput == "k") // varusmies
+            {
+                discount = 0.5;
+                Console.WriteLine($"Hinta on {ticketPrice - (ticketPrice * discount)}!");
+            }
+
+            else
+            {
+                Console.WriteLine("Oletko opiskelija? Kyllä/En");
+                userInput = Console.ReadLine();
+                if (userInput == "k") // opiskelija
+                {
+                    discount = 0.45;                    
+               }
+
+                Console.WriteLine("Oletko mtk jäsen? Kyllä/En");
+                userInput = Console.ReadLine();
+                if (userInput == "k") // mtk jäsen
+                {
+                    discount = discount + 0.15; // opiskelija + mtk jäsen alennus tai jompikumpi
+                }
+                Console.WriteLine($"Hinta on {ticketPrice - (ticketPrice * discount)}!"); // opiskelija hinta mtk jäsenyydellä, opiskelija hinta tai mtk jäsen hinta.
+            }
+
         }
     }
 }
