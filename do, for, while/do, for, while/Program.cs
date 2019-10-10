9f07fbc0f2e0641e11f31ad33487ce2621a627e0
justusjukkala <justus.jukkala@student.saimia.fi>
2019-10-10 11:28:22 +0300

    Do, for, while created

---
+++ do, for, while/do, for, while/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace do__for__while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa antamasi luvun kertoman!");
+            Console.WriteLine("syötä luku!");
+        }
+    }
+}
