4afa3c0c800658e60e13654147b3bc191955f3a6
justusjukkala <justus.jukkala@student.saimia.fi>
2019-10-15 10:48:50 +0300

    PasswordQueryExample Version 2 done

--- PasswordQueryExample/PasswordQueryExample2/Program.cs
+++ PasswordQueryExample/PasswordQueryExample2/Program.cs
@@ -6,7 +6,28 @@ namespace PasswordQueryExample2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string password = "qwerty1234";
+            Console.WriteLine("Salasanan kysely For-silmukalla");
+
+            for(int i = 0; i < 3; i++)
+            {
+                Console.Write($"{i+1}. " +
+                    $"Syötä salasana: ");
+                string userInput = Console.ReadLine();
+
+                if (userInput == password)
+                {
+                    Console.WriteLine("Salasana OIKEIN!");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Väärin meni! Yritä uudelleen");
+                }
+            }
+
+            Console.WriteLine("Tili LUKITTU!");
+
         }
     }
 }
