cf1c638c2ec363129eef7ceef4d70ccaec8c82d3
justusjukkala <justus.jukkala@student.saimia.fi>
2019-10-15 10:34:36 +0300

    PasswordQueryExample version 1 done

---
+++ PasswordQueryExample/PasswordQueryExample2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PasswordQueryExample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
