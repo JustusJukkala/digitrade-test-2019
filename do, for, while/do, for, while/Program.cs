d15babc382c453b4e253a78a1c3edd76a3e501e9
justusjukkala <justus.jukkala@student.saimia.fi>
2019-10-15 10:09:39 +0300

    Loop Example do, for, while 2 done

--- do, for, while/do, for, while/Program.cs
+++ do, for, while/do, for, while/Program.cs
@@ -8,6 +8,15 @@ namespace do__for__while
         {
             Console.WriteLine("Ohjelma tulostaa antamasi luvun kertoman!");
             Console.WriteLine("syötä luku!");
+            {
+                int i = int.Parse(Console.ReadLine()), sum = 0;
+                while (i <= 5)
+                {
+                    sum += i;
+                    i++;
+                }
+                Console.WriteLine("Sum = {0}", sum);
+            }
         }
     }
 }
