dd52e9604a280ac8e15bbb70bea469f677c51ae5
justusjukkala <justus.jukkala@student.saimia.fi>
2019-10-15 11:13:24 +0300

    Do, For, While in progress

--- do, for, while/do, for, while/Program.cs
+++ do, for, while/do, for, while/Program.cs
@@ -8,15 +8,26 @@ namespace do__for__while
         {
             Console.WriteLine("Ohjelma tulostaa antamasi luvun kertoman!");
             Console.WriteLine("syötä luku!");
+            int userInput = int.Parse(Console.ReadLine());
+
+            for (int n = 0; n < userInput; n++)
             {
-                int i = int.Parse(Console.ReadLine()), sum = 0;
-                while (i <= 5)
-                {
-                    sum += i;
-                    i++;
-                }
-                Console.WriteLine("Sum = {0}", sum);
+                Console.WriteLine
             }
+            if (n < 0)
+            {
+                Console.WriteLine("Virheellinen syöte!");
+            }
+            else
+            {
+                while (n > 0)
+                {
+                    fact = fact * n
+                    n = n - 1;
+                }
+            }
+            //Tulostaa vastauksen näytölle
+            Console.WriteLine("Vastaus: " + fact);
         }
     }
 }
