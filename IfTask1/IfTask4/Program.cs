f5448cbdc950c58d117bd5b086748349dc0a8e01
justusjukkala <justus.jukkala@student.saimia.fi>
2019-10-27 22:16:55 +0200

    IfTask4 completed

--- IfTask1/IfTask4/Program.cs
+++ IfTask1/IfTask4/Program.cs
@@ -7,17 +7,52 @@ namespace IfTask4
         static void Main(string[] args)
         {
             Console.WriteLine("Ohjelma lajittelee luvut nousevaan järjestykseen!");
-            Console.WriteLine("Syötä kolme kokonaislukua: ")
-            Console.WriteLine("Syötä kokonaisluku: ");
-            Console.WriteLine("Syötä kokonaisluku: ");
-            int userInputNumberx = int.Parse(Console.WriteLine());
-            int userInputNumbery = int.Parse(Console.WriteLine());
-            int userInputNumberz = int.Parse(Console.WriteLine());
+            // Tietojen kysely
+            Console.Write("Syötä 1. luku: ");
+            int x = int.Parse(Console.ReadLine());
+            
+            Console.Write("Syötä 2. luku: ");
+            int y = int.Parse(Console.ReadLine());
 
-            if (userInputNumberx > userInputNumbery)
+            Console.Write("Syötä 3. luku: ");
+            int z = int.Parse(Console.ReadLine());
+
+
+            // Logiikka, jolla ratkaistaan ongelma
+            if ((x > y) && (x > z))
+                //ehtojen asetus
             {
-                Console.WriteLine($"Luku {userInputNumberx} on suurempi kuin Luku{userInputNumbery}");
+                if (y > z)
+                //ehdolle vielä lisä ehto
+                {
+                    Console.WriteLine($" {x} , {y} , {z} ");
+                }
+
+                else if (z > y)
+                //Jos z on isompi kuin y
+                {
+                    Console.WriteLine($" {x} , {z} , {y} ");
+                }
+
             }
+            else if ((y > x) && (y > z))
+                //asettaa ehdot
+            {
+                if (x > z)
+                    //ehdon ehto
+                {
+                    Console.WriteLine($" {y} , {x} , {z} ");
+                    //tulostaa tämän jos ehdot toteutuvat
+                }
+
+                else if (z > x)
+                    //jos z on isompi kuin x
+                {
+                    Console.WriteLine($" {y} , {z} , {x} ");
+                    //tulostaa tämän muussa tapauksessa
+                }
+            }
+
         }
     }
 }
