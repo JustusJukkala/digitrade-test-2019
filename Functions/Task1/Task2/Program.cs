8bd95e582e3aea9f8a68f0c745224b8adc689bbf
justusjukkala <justus.jukkala@student.saimia.fi>
2019-10-28 13:05:49 +0200

    Functions Task2 created

---
+++ Functions/Task1/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma kertoo kumpi annetuista numeroista on pienempi");
+            Console.WriteLine("1. Numero: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("2. Numero: ");
+            int b = int.Parse(Console.ReadLine());
+        }
+
+        static void minimum()
+        {
+
+        }
+
+    }
+}
