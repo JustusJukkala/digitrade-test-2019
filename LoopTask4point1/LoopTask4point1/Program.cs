1879fa3a162bfac0069d295566bca270a234f665
justusjukkala <justus.jukkala@student.saimia.fi>
2019-10-30 11:30:01 +0200

    LoopTask 4.1 created

---
+++ LoopTask4point1/LoopTask4point1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoopTask4point1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tulostaa kokonaisluvut 1-10 ja niiden neliöjuuret");
+        }
+    }
+}
