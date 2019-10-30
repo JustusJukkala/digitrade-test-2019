b71237eb6601bbfc366528b7406c95c797e1000e
justusjukkala <justus.jukkala@student.saimia.fi>
2019-10-30 11:41:38 +0200

    LoopTask 4.2 created

---
+++ LoopTask4point1/LoopTask4point2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoopTask4point2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tulostaa kertotaulun luvuille 1-9");
+        }
+    }
+}
