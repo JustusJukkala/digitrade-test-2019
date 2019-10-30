f198cc6a376aa111fec50f151add3430ceca9478
justusjukkala <justus.jukkala@student.saimia.fi>
2019-10-30 11:37:42 +0200

    LoopTask 4.1 completed

--- LoopTask4point1/LoopTask4point1/Program.cs
+++ LoopTask4point1/LoopTask4point1/Program.cs
@@ -7,6 +7,12 @@ namespace LoopTask4point1
         static void Main(string[] args)
         {
             Console.WriteLine("Tulostaa kokonaisluvut 1-10 ja niiden neliöjuuret");
+
+            Console.WriteLine("Luku\tNeliöjuuri");
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{i + 1}\t{Math.Sqrt(i + 1)}");
+            }
         }
     }
 }
