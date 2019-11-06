cf0b671ff6715b840d948652c84bc46bf2b19a06
justusjukkala <justus.jukkala@student.saimia.fi>
2019-11-06 08:29:03 +0200

    LoopTask4point3 created

---
+++ IfTasks/LoopTask4point1/LoopTask4point3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoopTask4point3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
