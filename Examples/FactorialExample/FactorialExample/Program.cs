9a412c35395dba1951e174b10e01f1b423752e72
justusjukkala <justus.jukkala@student.saimia.fi>
2019-11-13 08:22:11 +0200

    ArrayExample Created

---
+++ Examples/FactorialExample/FactorialExample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace FactorialExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define variables
+            int n = 0;
+            int fact = 1;
+
+            // Tulostetaan viesti käyttäjälle
+            Console.WriteLine("Kertoman laskenta");
+            Console.Write("Minkä luvun kertoma lasketaan? ");
+
+            // Luetaan käyttäjän syöte ja pakotetaan se integer muotoon
+            n = int.Parse(Console.ReadLine());
+
+            // Ehto, jossa testataan käyttäjän syöttämän arvo
+            // Ohjelman logiikka
+            if(n < 0)
+            {
+                Console.WriteLine("Virheellinen syöte!");
+            }
+            else
+            {
+                // Silmukka, jossa lasketaan kertoman arvo
+                while (n > 0)
+                {
+                    fact = fact * n; // Lasketaan kertoma joka kierroksella uudestaan
+                    n = n - 1; // Vähennetään arvoa yhdellä
+                }
+                // Tulosta vastaus näytölle
+                Console.WriteLine("Vastaus: " + fact);
+            }
+        }
+    }
+}
