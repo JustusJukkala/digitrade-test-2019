134bfda4a5dc9ad5bef46aafbff705530984de59
justusjukkala <justus.jukkala@student.saimia.fi>
2019-11-15 08:41:06 +0200

    ArrayExample2 Created

---
+++ Examples/ArrayExample2/ArrayExample2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArrayExample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
