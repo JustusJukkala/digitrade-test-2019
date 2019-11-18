27c9eb2bc0a2567bf24c9e018ca71da745ca1974
justusjukkala <justus.jukkala@student.saimia.fi>
2019-11-18 15:37:08 +0200

    Array1 In Progress

--- Arrays/Array1/Array1/Program.cs
+++ Arrays/Array1/Array1/Program.cs
@@ -6,7 +6,22 @@ namespace Array1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma tulostaa tuotteiden nimet ja yhteishinnat");
+            
         }
+
+        static void Tables(int Arrays)
+        {
+            const double V1 = 7.96;
+            const double V2 = 72.0;
+            const double V3 = 99.90;
+
+            decimal[] productPrice = new decimal[] { V1, V2, V3 };
+
+            int[] productAmount = new int[] {1, 3, 2};
+
+        }
+
+        static decimal
     }
 }
