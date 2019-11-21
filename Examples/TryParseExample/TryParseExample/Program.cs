9519ddda53e39c0cd41a629a796944f5d96432ef
justusjukkala <justus.jukkala@student.saimia.fi>
2019-11-21 08:57:44 +0200

    TryParseExample Completed.

--- Examples/TryParseExample/TryParseExample/Program.cs
+++ Examples/TryParseExample/TryParseExample/Program.cs
@@ -6,7 +6,20 @@ namespace TryParseExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("TryParse Esimerkki");
+            string userInput = Console.ReadLine();
+
+            // tarkastetaan onko annettu syöte luku
+            bool isNumber = int.TryParse(userInput, out int userNumber);
+
+            if (isNumber)
+            {
+                Console.WriteLine($"Syöte '{userInput}' on luku {userNumber}");
+            }
+            else
+            {
+                Console.WriteLine($"Syöte '{userInput}' on merkkijono ja muuttujan userNumber arvo on {userNumber}");
+            }
         }
     }
 }
