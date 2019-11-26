226cd75fda22246a9b2a83b29924f1e18ab1dadf
justusjukkala <justus.jukkala@student.saimia.fi>
2019-11-26 11:15:34 +0200

    StringsTask2 Finished.

--- Strings/StringsTask/StringsTask2/Program.cs
+++ Strings/StringsTask/StringsTask2/Program.cs
@@ -7,6 +7,8 @@ namespace StringsTask2
         static void Main(string[] args)
         {
             Console.WriteLine("Ohjelma muuttaa syötetystä tekstistä e merkit @-merkeiksi.");
+            string message = Console.ReadLine();
+            Console.WriteLine($"{message}:sta tulee {message.Replace('e', '@')}");
         }
     }
 }
