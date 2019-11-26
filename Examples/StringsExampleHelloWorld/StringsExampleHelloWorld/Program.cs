f9b5521148a427cc6056e57b31c5b26ad357c1b0
justusjukkala <justus.jukkala@student.saimia.fi>
2019-11-26 10:32:38 +0200

    StringsExample print string values one by one

--- Examples/StringsExampleHelloWorld/StringsExampleHelloWorld/Program.cs
+++ Examples/StringsExampleHelloWorld/StringsExampleHelloWorld/Program.cs
@@ -6,7 +6,16 @@ namespace StringsExampleHelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Merkkijononkäsittely esimerkki");
+            string message = "Hello World!";
+
+            // Tulostetaan message -muuttujan merkit yksitellen näytölle
+            Console.WriteLine($"{message}:ssä on {message.Length} merkkiä.");
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                Console.WriteLine(message[i]);
+            }
         }
     }
 }
