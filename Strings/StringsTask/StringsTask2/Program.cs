41a20fb2247f438f450d0a280b14f88a69aad354
justusjukkala <justus.jukkala@student.saimia.fi>
2019-11-26 11:02:39 +0200

    StringTask2 created.

---
+++ Strings/StringsTask/StringsTask2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringsTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma muuttaa syötetystä tekstistä e merkit @-merkeiksi.");
+        }
+    }
+}
