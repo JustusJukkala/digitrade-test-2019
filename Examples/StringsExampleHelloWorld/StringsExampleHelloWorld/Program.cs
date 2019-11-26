11b78f3fa94adde0f0d8d29a12622c69ba0e7e4e
justusjukkala <justus.jukkala@student.saimia.fi>
2019-11-26 10:53:02 +0200

    StringsExample Done.

--- Examples/StringsExampleHelloWorld/StringsExampleHelloWorld/Program.cs
+++ Examples/StringsExampleHelloWorld/StringsExampleHelloWorld/Program.cs
@@ -7,14 +7,40 @@ namespace StringsExampleHelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Merkkijononkäsittely esimerkki");
-            string message = "Hello World!";
+            string message = "  Hello World!  ";
 
             // Tulostetaan message -muuttujan merkit yksitellen näytölle
             Console.WriteLine($"{message}:ssä on {message.Length} merkkiä.");
+            Console.WriteLine("---------");
 
-            for (int i = 0; i < message.Length; i++)
+            PrintMessage(message);
+            Console.WriteLine("----------");
+
+            // Poistetaan välilyönnit alusta ja lopusta
+            string tempMessage = message.Trim();
+            PrintMessage(tempMessage);
+            Console.WriteLine("----------");
+
+            tempMessage = tempMessage.ToUpper();
+            PrintMessage(tempMessage);
+            Console.WriteLine("----------");
+
+            // Etsitään merkin kohta merkkijonosta 
+            int position = tempMessage.IndexOf(' ');
+            Console.WriteLine($"Välilyönti löytyy kohdasta {position}");
+            //for (int i = 0; i < message.Length; i++)
+            //{
+            //    Console.WriteLine(message[i]);
+            //}
+
+
+        }
+
+        static void PrintMessage(string s)
+        {
+            for (int i = 0; i < s.Length; i++)
             {
-                Console.WriteLine(message[i]);
+                Console.WriteLine(s[i]);
             }
         }
     }
