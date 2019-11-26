0929825e935cb8f017e28bc163b8982b0cb4fe0d
justusjukkala <justus.jukkala@student.saimia.fi>
2019-11-26 10:59:46 +0200

    StringsTask1 Completed

---
+++ Strings/StringsTask/StringsTask/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StringsTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma ilmoittaa syötettyjen merkkien lukumäärän");
+            string message = Console.ReadLine();
+            Console.WriteLine($"{message}:ssä on {message.Length} merkkiä.");
+        }
+    }
+}
