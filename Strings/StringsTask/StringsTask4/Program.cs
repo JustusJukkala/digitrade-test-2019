ae90d2bd87df211983837f6134c519fedc110d31
justusjukkala <justus.jukkala@student.saimia.fi>
2019-11-28 11:13:08 +0200

    StringTask4 Completed.

--- Strings/StringsTask/StringsTask4/Program.cs
+++ Strings/StringsTask/StringsTask4/Program.cs
@@ -7,7 +7,32 @@ namespace StringsTask4
         static void Main(string[] args)
         {
             Console.WriteLine("Ohjelma kertoo, että onko antamasi sana tai lause palidromi.");
+            string message = Console.ReadLine();
+            message = message.Replace(" ", ""); // Poistetaan välilyönnit, jotta ohjelman on helpompi käsitellä lausetta/sanaa.
+            int deliver = 0;
+            int receive = message.Length - 1;
+            bool check = true; // bool:n avulla pystytään tarkistamaan, että  lause/sana on molemminpäin sama.
 
+            for (int i = 0; i < message.Length - 1; i++) // Tarkistaa, että lause/sana on sama oikein ja väärin päin.
+            {
+                if (message[deliver] == message[receive])
+                {
+                    deliver++;
+                    receive--;
+                }
+                else
+                    check = false;
+            }
+            if (check == true)
+            {
+                message = message.ToUpper();
+                Console.WriteLine($"Antamasi syöte {message} oli palindromi"); // Tulostaa tämän jos lause/sana on palindromi
+            }
+            else
+            {
+                message = message.ToUpper();
+                Console.WriteLine($"Antamasi syöte {message} ei ollut palindromi"); // Tulostaa tämän jos lause/sana ei ole palindromi
+            }
         }
     }
 }
