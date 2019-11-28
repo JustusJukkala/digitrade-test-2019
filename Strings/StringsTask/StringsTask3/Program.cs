cabfaee911a73d505bb27e896c05fc8d5f48915b
justusjukkala <justus.jukkala@student.saimia.fi>
2019-11-28 10:32:29 +0200

    StringsTask3 Completed.

--- Strings/StringsTask/StringsTask3/Program.cs
+++ Strings/StringsTask/StringsTask3/Program.cs
@@ -8,7 +8,15 @@ namespace StringsTask3
         {
             Console.WriteLine("Ohjelma laskee antamastasi syötteestä l-kirjaimien määrän");
             string message = Console.ReadLine();
-
+            int countL = 0;
+            for (int i = 0; i < message.Length; i++)
+            {
+                if (message[i] == 'l')
+                {
+                    countL++;
+                }
+            }
+            Console.WriteLine($"Syötteestä {message} löytyi {countL} l-kirjainta.");
         }
     }
 }
