812f350e99918c49980d88f113d4f0759679453d
justusjukkala <justus.jukkala@student.saimia.fi>
2019-11-28 10:39:16 +0200

    StringsTask4 Created.

---
+++ Strings/StringsTask/StringsTask4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StringsTask4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma kertoo, että onko antamasi sana tai lause palidromi.");
+
+        }
+    }
+}
