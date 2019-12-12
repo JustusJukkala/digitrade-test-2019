6854892f45e5f04ca5dfde37164fb4c105f0ea8a
justusjukkala <justus.jukkala@student.saimia.fi>
2019-12-12 12:55:50 +0200

    SocialSecurityNumber created.

---
+++ Strings/SocialSecurityNumber/SocialSecurityNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SocialSecurityNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tarkastaa Hetun oikeellisuuden.");
+        }
+    }
+}
